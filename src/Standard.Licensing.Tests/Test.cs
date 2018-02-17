5757415a89920a4ae32ff6dfa6b9a94eaa8e4549
Junian <git@junian.net>
2018-02-17 13:59:28 +0700

    add test project

---
+++ src/Standard.Licensing.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Standard.Licensing.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
